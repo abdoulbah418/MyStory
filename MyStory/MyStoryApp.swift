a5cd71a0e4189bd032216a035f3092ea8d8843f1
Abdoul Bah <abdoulbah418@gmail.com>
2023-03-10 23:03:52 -0500

    Initial Commit

---
+++ MyStory/MyStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryApp.swift
+//  MyStory
+//
+//  Created by Abdoul Bah on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
